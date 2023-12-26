7339d55a4dc6d472e2f8d6fb9918dedfc91489f5
lmy-py <77715423+lmy-py@users.noreply.github.com>
2023-12-26 10:38:07 +0800

    Update sidebar.tsx
    play

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          牧羊@NextChat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          <a href="https://play.limuyang.com.cn">Go Play. Please ask me if you need passward</a>
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
